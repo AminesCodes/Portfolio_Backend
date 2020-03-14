7966d681a2f52fd20cdaad2137795513889d1afe
AminesCodes <aminebensalem@pursuit.org>
2020-03-14 09:26:44 -0400

    Remove unwanted routes

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/contact', indexRouter);
 
 app.use('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+    response.send('Welcome to my portfolio');
 });
 
 
