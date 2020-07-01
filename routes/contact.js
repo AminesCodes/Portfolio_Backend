c79d421c4e9c998e4fb2b9cdb7218a77b8be05b2
AminesCodes <aminebensalem@pursuit.org>
2020-07-01 01:42:47 -0400

    Remove a nodemon verification

--- routes/contact.js
+++ routes/contact.js
@@ -11,13 +11,13 @@ const transport = {
 
 const transporter = nodemailer.createTransport(transport)
 
-transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take messages');
-    }
-});
+// transporter.verify((error, success) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Server is ready to take messages');
+//     }
+// });
 
 
 router.post('/send', (request, response) => {
